bcd4141c22936fb20c17d501e6edd74ec3eb561c
mmathabo_m <122876091+MmathaboM@users.noreply.github.com>
2024-07-16 20:27:43 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+    //Hero Slider
+    $('#hero-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        items:1,
+        dots: false,
+        smartSpeed :1000,
+        navtext: ['PREV','NEXT'],
+        responsive:{
+            0:{
+            
+            },
+            600:{
+                
+            },
+            1000:{
+                
+            }
+        }
+    })
+})
+
